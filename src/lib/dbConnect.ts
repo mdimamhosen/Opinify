0c8ae12a262a42fc3a962ffe4a0cef9d3d56e3fd
mdimamhosen <imamhosen9426224@gmail.com>
2024-08-29 22:34:21 +0600

    ok

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,26 +1,27 @@
 import mongoose from "mongoose";
 
- type ConnectionObject ={
-    isConnected?: number
- }
+type ConnectionObject = {
+  isConnected?: number;
+};
 
- const connection: ConnectionObject={
- };
+const connection: ConnectionObject = {};
 
- async function dbConnect() : Promise<void> {
-    if(connection.isConnected){
-        console.log("Using existing connection");
-          return;
-    }
-    try {
-        await mongoose.connect(process.env.MONGODB_URI || "", {
-
-        })
-        connection.isConnected = mongoose.connections[0].readyState;
-        console.log("New connection created");
-    } catch (error) {
-        console.log ("Error in dbConnect",error);
-        process.exit(1);
-    }
- }
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Using existing connection");
+    return;
+  }
+  try {
+    await mongoose.connect(
+      "mongodb+srv://imam:aaadsr191@cluster0.wahes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" ||
+        "",
+      {}
+    );
+    connection.isConnected = mongoose.connections[0].readyState;
+    console.log("New connection created");
+  } catch (error) {
+    console.log("Error in dbConnect", error);
+    process.exit(1);
+  }
+}
 export default dbConnect;
