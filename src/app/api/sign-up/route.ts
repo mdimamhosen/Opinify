3e6b708f3a466ef0f8e2ceb356a9aca229ad0ad0
mdimamhosen <imamhosen9426224@gmail.com>
2024-08-29 10:57:16 +0600

    ok

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,4 +1,3 @@
-import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 import dbConnect from "@/lib/dbConnect";
 import User from "@/model/User";
 import brcypt from "bcryptjs";
@@ -8,10 +7,12 @@ export async function POST(request: Request) {
 
   try {
     const { username, email, password } = await request.json();
+
     const isUserExistWithUsername = await User.findOne({
       username,
       isVerified: true,
     });
+
     if (isUserExistWithUsername) {
       return Response.json(
         { success: false, message: "Username already exists" },
@@ -20,9 +21,11 @@ export async function POST(request: Request) {
         }
       );
     }
+
     const isUserExistWithEmail = await User.findOne({
       email,
     });
+
     if (isUserExistWithEmail) {
       if (isUserExistWithEmail.isVerified) {
         return Response.json(
@@ -33,81 +36,40 @@ export async function POST(request: Request) {
         );
       } else {
         const hashedPassword = await brcypt.hash(password, 10);
-        const expiryDate = new Date();
-        expiryDate.setHours(expiryDate.getHours() + 1);
-        const verifyCode = Math.floor(
-          100000 + Math.random() * 900000
-        ).toString();
-        isUserExistWithEmail.verifyCode = verifyCode;
-        isUserExistWithEmail.verifyCodeExpire = expiryDate;
         isUserExistWithEmail.password = hashedPassword;
         await isUserExistWithEmail.save();
-        const emailResponse = await sendVerificationEmail(
-          email,
-          username,
-          verifyCode
+
+        return Response.json(
+          {
+            success: true,
+            message: "User registered successfully",
+          },
+          {
+            status: 200,
+          }
         );
-        console.log(emailResponse);
-        if (emailResponse.success) {
-          return Response.json(
-            {
-              success: true,
-              message: "User registered successfully, verification email sent",
-              emailResponse: emailResponse,
-            },
-            {
-              status: 200,
-            }
-          );
-        } else {
-          return Response.json(
-            { success: false, message: emailResponse.message },
-            {
-              status: 500,
-            }
-          );
-        }
       }
     } else {
       const hashedPassword = await brcypt.hash(password, 10);
-      const expiryDate = new Date();
-      expiryDate.setHours(expiryDate.getHours() + 1);
-      const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
       const newUser = new User({
         username,
         email,
         password: hashedPassword,
-        verifyCode: verifyCode,
-        verifyCodeExpire: expiryDate,
         isAcceptingMessage: true,
         isVerified: true,
         messages: [],
       });
       await newUser.save();
 
-      const emailResponse = await sendVerificationEmail(
-        email,
-        username,
-        verifyCode
+      return Response.json(
+        {
+          success: true,
+          message: "User registered successfully",
+        },
+        {
+          status: 200,
+        }
       );
-      if (emailResponse.success) {
-        return Response.json(
-          {
-            success: true,
-            message: "User registered successfully, verification email sent",
-          },
-          {
-            status: 200,
-          }
-        );
-      } else {
-        return Response.json(
-          { success: false, message: emailResponse.message },
-          {
-            status: 500,
-          }
-        );
-      }
     }
   } catch (error) {
     console.error("Error registering user", error);
--- src/lib/Resend.ts
+++ src/lib/Resend.ts
@@ -1,3 +1,3 @@
 import { Resend } from "resend";
 
-export const resend = new Resend(process.env.RESEND_API_KEY);
+export const resend = new Resend("re_3kon9mYX_AU5xQgn2uEW4FeArnRtQB54f");
